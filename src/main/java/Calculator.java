f43054e3562aced2ee0e71e37ebfd43a41d61710
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-10-10 18:00:44 +0200

    Addition and Substraction implemented

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator {
+    public static double add_numbers(double a, double b) {
+        return a + b;
+    }
+    public static double sub_numbers(double a, double b) {
+        return a - b;
+    }
+}
